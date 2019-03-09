417a11a6bccf100e47a2b72f04fba524cf2d8f79
REchegorri <rodrigo_echegorri@yahoo.com.br>
2019-03-09 18:10:27 -0300

    Inserção local de dados, gerar datetime

--- venv/Controller/DataImport.py
+++ venv/Controller/DataImport.py
@@ -2,6 +2,7 @@ from glob import glob
 import os
 import csv
 import datetime
+import time
 
 '''
 Buscar arquivos de pasta do sistema
@@ -12,7 +13,26 @@ arq_estacao='C:\\Users\Livnick\Documents\dadosFocos\DadosEstacao.2017-07-01.2018
 arq_focos='C:\\Users\Livnick\Documents\dadosFocos\Focos.2017-07-01.2018-07-01.csv'
 estacao_reader = csv.DictReader(open(arq_estacao), delimiter = ';')
 focos_reader = csv.DictReader(open(arq_focos), delimiter = ';')
+estacao_list = []
+
 for input in estacao_reader:
+    '''Indexar dados em datetime'''
     data_aux = input['Data'].split('/')
-    dt = datetime.datetime(day=int(data_aux[0]), month=int(data_aux[1]), year=int(data_aux[2]),hour=int(input['Hora'][0:2]), minute=int(input['Hora'][2:4]))
-    print(dt)
+    dt = time.mktime(datetime.datetime(day=int(data_aux[0]), month=int(data_aux[1]), year=int(data_aux[2]),hour=int(input['Hora'][0:2]), minute=int(input['Hora'][2:4])).timetuple())
+    data_dict = {'Datetime': dt}
+    data_dict['TempBulboSeco'] = input['TempBulboSeco']
+    data_dict['TempBulboUmido'] = input['TempBulboUmido']
+    data_dict['UmidadeRelativa'] = input['UmidadeRelativa']
+    data_dict['PressaoAtmEstacao'] = input['PressaoAtmEstacao']
+    data_dict['DirecaoVento'] =  input['DirecaoVento']
+    data_dict['VelocidadeVentoNebulosidade'] =  input['VelocidadeVentoNebulosidade']
+    estacao_list.append(data_dict)
+
+for input in focos_reader:
+    data_dict = {'RiscoFog': input['RiscoFog']}
+    '''model 2017/07/03 16:53:00'''
+    dt = time.mktime(datetime.datetime.strptime(input['DataHora'], "%Y/%m/%d %H:%M:%S").timetuple())
+    data_dict['Datetime'] = dt
+    data_dict['Latitude'] = input['Latitude']
+    data_dict['Longitude'] = input['Longitud']
+    print(data_dict)
